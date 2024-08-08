cd43ca99f17d1cca47a217ab3b70b4ec8e5fe9fd
0xc0ffee1 <0xc0ffee1@users.noreply.github.com>
2024-08-09 01:05:06 +0300

    fix provider

--- rust/server/src/server.rs
+++ rust/server/src/server.rs
@@ -66,7 +66,7 @@ pub struct Opt {
 
 /// Returns default server configuration along with its certificate.
 fn configure_server() -> anyhow::Result<ServerConfig> {
-    rustls::crypto::ring::default_provider().install_default().expect("Failed to install rustls crypto provider");
+    let _ = rustls::crypto::ring::default_provider().install_default().expect("Failed to install rustls crypto provider");
     let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
 
     let cert_der = CertificateDer::from(cert.cert);
