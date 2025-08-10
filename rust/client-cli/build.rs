f7f52e95d00a95324f55cbbc4953bd7067d3c1d5
0xc0ffee1 <0xc0ffee1@users.noreply.github.com>
2025-08-10 20:15:17 +0300

    0.4.0

--- rust/client-cli/build.rs
+++ rust/client-cli/build.rs
@@ -1,9 +1,23 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .compile(
-            &["../../proto/client_ipc.proto"],
-            &["../../proto"]
-        )?;
-    Ok(())
+    // Try multiple possible locations for proto files
+    let proto_paths = vec![
+        ("../../proto/client_ipc.proto", "../../proto"),  // Local development
+        ("../proto/client_ipc.proto", "../proto"),        // Alternative structure
+        ("/proto/client_ipc.proto", "/proto"),            // Docker/Cross build
+    ];
+
+    for (proto_file, proto_dir) in proto_paths {
+        if std::path::Path::new(proto_file).exists() {
+            tonic_build::configure()
+                .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+                .compile(
+                    &[proto_file],
+                    &[proto_dir]
+                )?;
+            return Ok(());
+        }
+    }
+
+    // If we get here, proto files weren't found in any expected location
+    panic!("Proto files not found. Tried: ../../proto, ../proto, and /proto");
 }
